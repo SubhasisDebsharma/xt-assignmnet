b2f4d82380e1203471d4ac49360bdd141155357d
Subhasis Debsharma(IN4424) <M1044424@mindtree.com>
2020-11-17 11:50:43 +0530

    removed unsed import

--- src/app/modules/launches/components/launches/launches.component.ts
+++ src/app/modules/launches/components/launches/launches.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { ActivatedRoute, Params } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 import { Subscription } from 'rxjs';
 import { IEventDetailsModel } from '../../models/event-detail.model';
 
